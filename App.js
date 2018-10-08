ed237ac7facfeefedbeeeccc4b4f90469a522ba6
Daniel Felipe Montoya Ramirez <dmontoya@apptitud.com.co>
2018-10-07 21:46:41 -0500

    Clase 18. AsyncStorage

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet, Text, View, AsyncStorage, Button,
+} from 'react-native';
 import Header from './Header';
 import Body from './Body';
 
@@ -12,22 +14,56 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    this.retrievePhone();
+  }
+
   pushText = (value) => {
     this.setState({ text: value });
   }
 
   agregarTarea = () => {
+    const newTasks = [...this.state.tasks, { text: this.state.text, key: Date.now() }];
+    this.savePhone(newTasks);
     this.setState({
       text: '',
-      tasks: [...this.state.tasks, { text: this.state.text, key: Date.now() }],
+      tasks: newTasks,
     });
   }
 
   deleteTask = (id) => {
     const newTasks = this.state.tasks.filter(task => task.key !== id);
+    this.savePhone(newTasks);
     this.setState({
       tasks: newTasks,
     });
+  }
+
+  savePhone = (tasks) => {
+    AsyncStorage.setItem('@AppCurso:tasks', JSON.stringify(tasks))
+      .then((value) => {
+        console.log(value);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  retrievePhone = () => {
+    AsyncStorage.getItem('@AppCurso:tasks')
+      .then((value) => {
+        console.log(value);
+        console.log(JSON.parse(value));
+        if (value !== null) {
+          const newTasks = JSON.parse(value);
+          this.setState({
+            tasks: newTasks,
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   render() {
